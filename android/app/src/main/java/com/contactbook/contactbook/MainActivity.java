7869a5dae4e29ea49e70fb45eb8d013c13b6663c
abhij <abhiramm95@gmail.com>
2024-01-14 21:48:46 +0530

    Initial commit

---
+++ android/app/src/main/java/com/contactbook/contactbook/MainActivity.java
@@ -0,0 +1,6 @@
+package com.contactbook.contactbook;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:contactbook/phone_book.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Contact_Book(),);
+  }
+}
